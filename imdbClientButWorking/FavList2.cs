af0860845f7eab42bec4bca8d2d4b96645616d31
Ravenaw <58209261+Ravenaw@users.noreply.github.com>
2022-06-03 21:00:51 +0200

    -

--- imdbClientButWorking/FavList2.cs
+++ imdbClientButWorking/FavList2.cs
@@ -7,19 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IMDbApiLib.Models;
 
 namespace imdbClientButWorking
 {
     public partial class FavList2 : Form
     {
-        public FavList2()
+        public FavList2(int userId)
         {
             InitializeComponent();
+            DatabaseController databaseController = new DatabaseController();
+            List<TitleData> favoriteList = new List<TitleData>(databaseController.GetFavListAsync(userId).Result);
+            
+            dataGridView2.DataSource=favoriteList;
+            dataGridView2.Columns[0].Visible = false;
+            dataGridView2.Columns[dataGridView2.ColumnCount - 1].Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             //search by title
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -35,11 +43,13 @@ namespace imdbClientButWorking
         private void dataGridView1_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
         {
             //add to favorites
+            dataGridView2.Rows.Add(dataGridView1.Rows[e.RowIndex]);
         }
 
         private void dataGridView2_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
         {
             //remove from favorites
+            dataGridView2.Rows.Remove(dataGridView2.Rows[e.RowIndex]);
         }
     }
 }
